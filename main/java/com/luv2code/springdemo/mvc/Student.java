0340236f625d7bdba00a4c8805309d613e5dcb97
Ray Guo <siruiguo@outlook.com>
2021-05-17 19:22:11 +0800

    radio buttons

--- main/java/com/luv2code/springdemo/mvc/Student.java
+++ main/java/com/luv2code/springdemo/mvc/Student.java
@@ -8,6 +8,7 @@ public class Student {
 	private String lastName;
 	private String country;
 	private LinkedHashMap<String, String> countryOptions;
+	private String favoriteLanguage;
 	
 	public Student() {
 		countryOptions = new LinkedHashMap<>();
@@ -44,6 +45,14 @@ public class Student {
 
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
 	}
 	
 	
